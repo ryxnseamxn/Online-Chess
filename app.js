3c58d04fe9b5aced553831cb9773de569be668c4
Ryan Seaman <77586863+ryxnseamxn@users.noreply.github.com>
2024-07-30 22:21:27 -0500

    changed default route and deleted old node files (#22)

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path'); 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, 'public/pages/login.html')); 
 })
 
 app.listen(port, () => {
+  console.log(path.join(__dirname, 'public/pages/login.html'));
   console.log(`Example app listening on port ${port}`)
 })
 
