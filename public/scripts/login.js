66655b9df60a22b82361190d88ff31d51e06c4c4
ryxnseamxn <ryan.l.seaman@gmail.com>
2024-08-01 23:39:07 -0500

    started adding login functionality

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,4 +1,14 @@
-let login = () => {
-    var currentUrl = document.location.href;
-    console.log(currentUrl); 
+let login = async () => {
+    const email = document.querySelector('#username').value; 
+    const password = document.querySelector('#password').value; 
+
+    const response = await fetch('/public/pages/login.html', {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password })
+    });
+
+    console.log(response); 
 }
