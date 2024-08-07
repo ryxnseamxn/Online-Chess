d29ba831130942b5ab99ffdd4257f296045e58d5
Ryan Seaman <77586863+ryxnseamxn@users.noreply.github.com>
2024-08-07 13:33:51 -0500

    Chess (#30)
    * added chessboard using chessboard.js api
    
    * pain

--- public/scripts/chess.js
+++ public/scripts/chess.js
@@ -1,32 +1,5 @@
-const chessSquareArray = []; 
-
-function renderSquares(){
-    let numberOfSquares = 0; 
-    const chessBoard = document.querySelector(".chessBoard"); 
-    while (numberOfSquares < 64){
-        chessSquare = document.createElement("div"); 
-        chessSquare.classList.add("chessSquare"); 
-        chessSquareArray.push(chessSquare); 
-        numberOfSquares+=1; 
-    }
-    chessSquareArray.forEach((element, i) => {
-        determineSquareColor(element, i); 
-
-    })
-    return chessSquareArray.forEach((element) => {
-        return chessBoard.append(element); 
-    })
+var config = {
+    pieceTheme: '/img/chesspieces/wikipedia/{piece}.png',
+    position: 'start'
 }
-
-function determineSquareColor(element, i){
-    console.log((Math.floor(i/8))%2);
-    if((Math.floor(i/8))%2===0 && i % 2 ===0){
-        return element.style.backgroundColor = "#FFF";
-    }
-    if((Math.floor(i/8))%2 !==0 && i % 2 !==0){
-        return element.style.backgroundColor ="#FFF";
-    }
-    return element.style.backgroundColor = "#000"; 
-}
-
-renderSquares(); 
+var board1 = Chessboard('board1', config);
