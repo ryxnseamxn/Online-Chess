9b2c77a0c308a755695987b9b6b41baa2a426498
Ryan Seaman <77586863+ryxnseamxn@users.noreply.github.com>
2024-08-10 23:27:02 -0500

    bug fix (#37)

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,7 +1,10 @@
 let login = async () => {
     const emailField = document.querySelector('#username').value; 
     const passwordField = document.querySelector('#password').value; 
-
+    if(emailField.length <= 4 || passwordField.length <=4 ){
+        alert('Username and password must be greater than 4 characters'); 
+        return; 
+    }
     const response = await fetch('/pages/login.html', {
         method: 'POST', 
         headers: {
--- public/scripts/signup.js
+++ public/scripts/signup.js
@@ -1,7 +1,10 @@
 let signup = async () => {
     const emailField = document.querySelector('#username').value; 
-    const passwordField = document.querySelector('#password').value; 
-
+    const passwordField = document.querySelector('#password ').value; 
+    if(emailField.length <= 4 || passwordField.length <=4 ){
+        alert('Username and password must be greater than 4 characters'); 
+        return; 
+    }
     const response = await fetch('/pages/signup.html', {
         method: 'POST', 
         headers: {
@@ -13,6 +16,7 @@ let signup = async () => {
     const { success, message } = await response.json(); 
     if(success){
         window.location.href = '/pages/login.html'; 
+        console.log(`success: ${success}`); 
         alert('Signup Successful!')
     }
     alert('Email Already Exists!'); 
