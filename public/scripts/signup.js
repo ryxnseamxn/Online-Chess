d7f32836cf3eda09f43128b4d6b94de760824071
Ryan Seaman <ryan.l.seaman@gmail.com>
2024-08-22 22:45:50 -0500

    wip: bug fixes

--- public/scripts/signup.js
+++ public/scripts/signup.js
@@ -1,11 +1,11 @@
 let signup = async () => {
     const emailField = document.querySelector('#username').value; 
-    const passwordField = document.querySelector('#password ').value; 
+    const passwordField = document.querySelector('#password').value; 
     if(emailField.length <= 4 || passwordField.length <=4 ){
         alert('Username and password must be greater than 4 characters'); 
         return; 
     }
-    const response = await fetch('/pages/signup.html', {
+    const response = await fetch('signup.html', {
         method: 'POST', 
         headers: {
             'Content-Type': 'application/json'
