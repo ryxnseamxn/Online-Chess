5059743270ad6d13c6f30ba3a0427c1e63dadaee
Ryan Seaman <ryan.l.seaman@gmail.com>
2024-08-24 22:54:13 -0500

    fixed signup endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -33,13 +33,11 @@ router.get('/protected', protected, async (req, res) => {
 })
 
 //signup logic 
-router.post("/pages/signup.html", async (req, res) => {   
-    console.log(`Auth.js endpoint`); 
+router.post("/pages/signup", async (req, res) => {   
     try {
         const {email, password } = req.body; 
-        console.log(email, password); 
         //check if user exists 
-        let user = db.getUsers(email, password); 
+        let user = await db.getUsers(email, password);         
         if(user){
             return res.status(500).json({
                 message: 'User already exists',
@@ -47,7 +45,7 @@ router.post("/pages/signup.html", async (req, res) => {
             }); 
         }
         //if the user doesn't exist, create a new one 
-        let result = db.newUser(email, password); 
+        let result = await db.newUser(email, password); 
         res.status(200).json({
             message: 'User created', 
             type: 'success',
